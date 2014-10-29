39ff6828821c1cb0924bd26237f15f338e26ff18
Christophe Simon <christophe.simon@dailymotion.com>
2014-10-29 18:50:33 +0100

    Lsync integration + rows im urxvt + i3lock tiled.

--- rc.lua
+++ rc.lua
@@ -62,7 +62,7 @@ editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Lock screen command
-xlock = string.format("i3lock -i %s", beautiful.lockscreen)
+xlock = string.format("i3lock -t -i %s", beautiful.lockscreen)
 
 -- Default modkey.
 -- Usually, Mod4 is the key with a logo between Control and Alt.
