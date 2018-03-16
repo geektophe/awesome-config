3587441da0d6b8baff849e9a6b310273bd3edab0
Christophe Simon <geektophe@gmail.com>
2018-03-16 10:54:56 +0100

    Fixed urxvt character set issue

--- rc.lua
+++ rc.lua
@@ -56,7 +56,7 @@ themename = "gits"
 
 -- This is used later as the default terminal and editor to run.
 -- terminal = "x-terminal-emulator"
-terminal = "xterm"
+terminal = "urxvt"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
